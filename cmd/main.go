65e1646fac73826d769b59ad025da37a5bb4b4d6
Oleg_Dzhur <olegdjur@gmail.com>
2023-02-19 13:53:51 +0600

    craeted func main

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+
+	"github.com/OlegDjur/go-grpc-api-gateway/pkg/auth"
+	"github.com/OlegDjur/go-grpc-api-gateway/pkg/config"
+	"github.com/OlegDjur/go-grpc-api-gateway/pkg/order"
+	"github.com/OlegDjur/go-grpc-api-gateway/pkg/product"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	r := gin.Default()
+
+	authSvc := *auth.RegisterRoutes(r, &c)
+	product.RegisterRoutes(r, &c, &authSvc)
+	order.RegisterRoutes(r, &c, &authSvc)
+
+	r.Run(c.Port)
+}
