bedf0ace95d842874f2c35b353310ad2e7332457
Oleg_Dzhur <olegdjur@gmail.com>
2023-02-19 13:42:29 +0600

    craeted service client product

--- pkg/product/routes/find_one.go
+++ pkg/product/routes/find_one.go
@@ -1,1 +1,24 @@
 package routes
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+
+	"github.com/OlegDjur/go-grpc-api-gateway/pkg/product/pb"
+	"github.com/gin-gonic/gin"
+)
+
+func FindOne(ctx *gin.Context, c pb.ProductServiceClient) {
+	id, _ := strconv.ParseInt(ctx.Param("id"), 10, 32)
+
+	res, err := c.FindeOne(context.Background(), &pb.FindOneRequest{
+		Id: int64(id),
+	})
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadGateway, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, &res)
+}
