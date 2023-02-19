4a2fe626ca747ac2114b5896328010df53e07ef1
Oleg_Dzhur <olegdjur@gmail.com>
2023-02-19 13:18:43 +0600

    initialized routes for order

--- pkg/order/client.go
+++ pkg/order/client.go
@@ -1,1 +1,22 @@
 package order
+
+import (
+	"fmt"
+
+	"github.com/OlegDjur/go-grpc-api-gateway/pkg/config"
+	"github.com/OlegDjur/go-grpc-api-gateway/pkg/order/pb"
+	"google.golang.org/grpc"
+)
+
+type ServiceClient struct {
+	Client pb.OrderServiceClient
+}
+
+func InitServiceClient(c *config.Config) pb.OrderServiceClient {
+	cc, err := grpc.Dial(c.OrderSvcUrl, grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("Cloud not connect:", err)
+	}
+
+	return pb.NewOrderServiceClient(cc)
+}
